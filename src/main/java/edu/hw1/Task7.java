9f7ebc30ddd69660ecc58d9a2b64e16841612cc4
Lalisa <komendantova02@mail.ru>
2023-10-16 18:56:11 +0300

    Реализация сдвига вправо через сдвиг влево

--- src/main/java/edu/hw1/Task7.java
+++ src/main/java/edu/hw1/Task7.java
@@ -42,36 +42,8 @@ public class Task7 {
     }
 
     public static int rotateRight(int n, int shift) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-
-        if (n < 0 || shift < 0) {
-            return -1;
-        }
-
-        if (shift == 0) {
-            return n;
-        }
-
-        String nStr = Integer.toBinaryString(n);
-        char[] nChar = nStr.toCharArray();
-
-        int newShift;
-        if (shift > nChar.length) {
-            newShift = shift % nChar.length;
-        } else {
-            newShift = shift;
-        }
-
-        for (int i = 0; i < newShift; i++) {
-            char tmp = nChar[nChar.length - 1];
-            for (int j = nChar.length - 1; j > 0; j--) {
-                nChar[j] = nChar[j - 1];
-            }
-            nChar[0] = tmp;
-        }
-
-        return Integer.parseInt(new String(nChar), 2);
+          String nStr = Integer.toBinaryString(n);
+          int shiftL = nStr.length() - shift;
+          return rotateLeft(n, shiftL);
     }
 }
